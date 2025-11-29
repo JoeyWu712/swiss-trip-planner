f8959762c4f163f80bea4f10f310045ea9c7afd8
JoeyWu712 <r98522305@gmail.com>
2025-11-30 01:32:56 +0800

    Fix: Final PostCSS configuration update

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,9 @@
+// postcss.config.js
 /** @type {import('postcss-load-config').Config} */
 export default {
-  // PostCSS 外掛程式配置
-  plugins: {
-    // 啟用 Tailwind CSS 引擎，這是編譯 Tailwind 樣式所必需的
-    tailwindcss: {},
-    // 啟用 Autoprefixer，自動為 CSS 屬性添加瀏覽器前綴（例如 -webkit-, -moz- 等）
-    autoprefixer: {},
-  },
+  // 將插件定義在陣列中，這是與最新 PostCSS/Vite 兼容的寫法
+  plugins: [
+    'tailwindcss', 
+    'autoprefixer',
+  ]
 }
