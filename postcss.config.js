8c3c8a89659d0f80483d31196e1e9358801de12d
JoeyWu712 <r98522305@gmail.com>
2025-11-30 01:01:53 +0800

    Add PostCSS config for Vercel build fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
-// postcss.config.js
+/** @type {import('postcss-load-config').Config} */
 export default {
+  // PostCSS 外掛程式配置
   plugins: {
-    // 修正: 使用 Vercel 要求的 @tailwindcss/postcss 獨立插件
-    '@tailwindcss/postcss': {},
+    // 啟用 Tailwind CSS 引擎，這是編譯 Tailwind 樣式所必需的
+    tailwindcss: {},
+    // 啟用 Autoprefixer，自動為 CSS 屬性添加瀏覽器前綴（例如 -webkit-, -moz- 等）
     autoprefixer: {},
   },
 }
